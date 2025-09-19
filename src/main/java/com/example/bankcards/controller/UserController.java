23171db7127f4352ea40911bdea3dd9ab2bee2a7
Nuby <nuby.4register@gmail.com>
2025-09-19 12:10:43 +0300

    add: User controller

---
+++ src/main/java/com/example/bankcards/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.example.bankcards.controller;
+
+import com.example.bankcards.entity.User;
+import com.example.bankcards.service.UserService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<User> getUserById(@PathVariable Long id) {
+        return userService.findAll().stream()
+                .filter(user -> user.getId().equals(id))
+                .findFirst();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.save(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteById(id);
+    }
+}
